c8b3ff172fb86c95b196b2d85e44d978b69ce997
Malcolm Smalls <malcolmsmalls.dev@gmail.com>
2024-02-28 02:10:59 -0800

    Finish mobile nav bar

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const nav = document.querySelector('.right-div-wrapper')
 const openNav = document.querySelector('.nav-btn')
+const closeNav = document.querySelector('.close-nav-btn')
 
 
 openNav.addEventListener('click', ()=> {
     nav.classList.add('navigation-open')
+})
+
+closeNav.addEventListener('click', ()=> {
+    nav.classList.remove('navigation-open')
 })
