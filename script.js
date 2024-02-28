6358fb37afb3f7fb3f06bf82a059ead7e9541004
Malcolm Smalls <malcolmsmalls.dev@gmail.com>
2024-02-28 01:46:16 -0800

    Allow for slideout mobile nav. Begin javaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector('.right-div-wrapper')
+const openNav = document.querySelector('.nav-btn')
+
+
+openNav.addEventListener('click', ()=> {
+    nav.classList.add('navigation-open')
+})
